ad993e9c1ab09b74a3a6555695b95906e2573c4a
nargo <nargo@credengine.onmicrosoft.com>
2022-01-14 16:26:53 -0600

    Added SearchV2

--- NavyRRL/Controllers/SearchController.cs
+++ NavyRRL/Controllers/SearchController.cs
@@ -6,12 +6,20 @@ using System.Web.Mvc;
 
 namespace NavyRRL.Controllers
 {
-    public class SearchController : Controller
+    public class SearchController : BaseController
     {
         // GET: Search
         public ActionResult Index()
         {
             return View( "~/views/search/searchv1.cshtml" );
         }
+		//
+
+		public ActionResult SearchV2()
+		{
+			return View( "~/views/search/searchv2.cshtml" );
+		}
+		//
+
     }
 }
