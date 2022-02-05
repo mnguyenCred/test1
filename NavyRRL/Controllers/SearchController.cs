fd888d4a428c3071310f686d305011230703739f
Michael Parsons <58825866+mparsons-ce@users.noreply.github.com>
2022-02-05 00:28:05 -0600

    Restred old search to use for testing while TaskSearch is being developed; Added Job and CCA filters to old search

---
+++ NavyRRL/Controllers/SearchController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AM = Models.Application;
+using SM = Models.Search;
+using Services;
+
+namespace NavyRRL.Controllers
+{
+    public class SearchController : BaseController
+    {
+		[Authorize( Roles = "Administrator, Site Staff" )]
+		public ActionResult Index()
+        {
+			if (!AccountServices.IsUserAuthenticated())
+            {
+				AM.SiteMessage siteMessage = new AM.SiteMessage()
+				{
+					Title = "Invalid Request",
+					Message = "You must be authenticated and authorized to use this feature"
+				};
+			}
+            return View( "~/views/Search/Searchv2.cshtml" );
+        }
+		//
+		
+		public ActionResult SearchV1()
+		{
+			return View( "~/views/Search/Searchv1.cshtml" );
+		}
+		//
+
+		public ActionResult SearchV2()
+		{
+			return View( "~/views/Search/Searchv2.cshtml" );
+		}
+		//
+
+		[HttpPost]
+		public ActionResult MainSearch( SM.SearchQuery query )
+		{
+			bool valid = true;
+			string status = "";
+			var results = new SearchServices().MainSearch( query, ref valid, ref status );
+
+			return JsonResponse( results, valid, new List<string>() { status }, null );
+		}
+		//
+    }
+}
--- Services/RatingServices.cs
+++ Services/RatingServices.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using AppEntity = Models.Schema.Rating;
+using Models.Schema;
 using Factories;
 namespace Services
 {
@@ -22,6 +23,20 @@ namespace Services
             var allRatings = RatingManager.GetAll( true ).OrderByDescending( m => m.CodedNotation.ToLower() == "all" ).ToList();
 
             return allRatings; ;
+        }
+
+        public static List<BilletTitle> GetAllActiveBilletTitles()
+        {
+            var output = JobManager.GetAll().ToList();
+
+            return output;
+        }
+
+        public static List<Organization> GetAllOrganizations()
+        {
+            var output = OrganizationManager.GetAll().ToList();
+
+            return output;
         }
     }
 }
